26421dc32699ec28fe4eeedb6fd4e6f8db7f28f5
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2020-04-19 20:31:33 +0200

    Get rotations and wolf movement right

--- Doggo/Doggo/Assets/Scripts/SheepController.cs
+++ Doggo/Doggo/Assets/Scripts/SheepController.cs
@@ -9,6 +9,8 @@ public class SheepController : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     private Vector3 destination = Vector3.zero;
 
+    private float speed = 0.5f;
+
     private float unitSize;
 
     // Start is called before the first frame update
@@ -23,17 +25,28 @@ public class SheepController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Map bounds
+        int minX = -5;
+        int minZ = -9;
+        int maxX = 30;
+        int maxZ = 22;
+
         //No destination set
         if(destination == Vector3.zero)
         {
             int offsetX = Random.Range(-2, 2);
             int offsetZ = Random.Range(-2, 2);
 
-            destination = new Vector3(gameObject.transform.position.x + offsetX, gameObject.transform.position.y, gameObject.transform.position.z + offsetZ);
+            int x = (int) Mathf.Max(Mathf.Min(gameObject.transform.position.x + offsetX, maxX), minX);
+            int z = (int)Mathf.Max(Mathf.Min(gameObject.transform.position.z + offsetZ, maxZ), minZ);
+
+            destination = new Vector3(x, gameObject.transform.position.y, z);
             navMeshAgent.isStopped = false;
             navMeshAgent.SetDestination(destination);
             PlayRunningAnimation();
         }
+
+        TurnTowardsTarget(destination);
 
         if (!navMeshAgent.pathPending)
         {
@@ -49,6 +62,24 @@ public class SheepController : MonoBehaviour
         }
     }
 
+    private void TurnTowardsTarget(Vector3 destination)
+    {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = destination - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = speed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
     private void PlayRunningAnimation()
     {
         sheepAnimator.SetTrigger("Run");
@@ -59,5 +90,15 @@ public class SheepController : MonoBehaviour
     {
         sheepAnimator.SetTrigger("Stop");
         sheepAnimator.ResetTrigger("Run");
+    }
+
+    public bool IsDead()
+    {
+        return false;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return gameObject.transform.position;
     }
 }
---
+++ Doggo/Doggo/Assets/Scripts/WolfController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WolfController : MonoBehaviour
+{
+    private SheepController target = null;
+    private NavMeshAgent navMeshAgent;
+
+    private Animator wolfAnimator;
+
+    private float speed = 0.5f;
+
+    private float unitSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateUpAxis = false;
+
+        wolfAnimator = GetComponent<Animator>();
+        unitSize = GetComponent<Collider>().bounds.size.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null || target.IsDead())
+        {
+            SheepController[] sheep = FindObjectsOfType<SheepController>();
+            target = sheep[Random.Range(0, sheep.Length)];
+        }
+
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(target.GetPosition());
+        PlayRunningAnimation();
+
+        TurnTowardsTarget();
+
+        if (!navMeshAgent.pathPending)
+        {
+            if (navMeshAgent.remainingDistance <= (unitSize + navMeshAgent.stoppingDistance))
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude <= 0.6f)
+                {
+                    navMeshAgent.isStopped = true;
+                    target = null;
+                    StopRunningAnimation();
+                }
+            }
+        }
+    }
+
+    private void TurnTowardsTarget()
+    {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = target.GetPosition() - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = speed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, -targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void PlayRunningAnimation()
+    {
+        wolfAnimator.SetTrigger("Run");
+        wolfAnimator.ResetTrigger("Stop");
+    }
+
+    private void StopRunningAnimation()
+    {
+        wolfAnimator.SetTrigger("Stop");
+        wolfAnimator.ResetTrigger("Run");
+    }
+}
