fa4ad39f32988a331dfcc8f98d81e015a7bc4086
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2020-04-19 20:45:47 +0200

    Implemented wolf spawns

---
+++ Doggo/Doggo/Assets/Scripts/WolfSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private float millisTillSpawn = 3000;
+
+    [SerializeField]
+    private GameObject enemyPrefab = null;
+
+    private float accumulatedDelta = 0;
+
+    private WolfSpawn[] wolfSpawns;
+
+    private void Start()
+    {
+        wolfSpawns = FindObjectsOfType<WolfSpawn>();
+    }
+
+    void Update()
+    {
+        float timeChangeInMillis = Time.deltaTime * 1000;
+        accumulatedDelta += timeChangeInMillis;
+
+        if (accumulatedDelta > millisTillSpawn)
+        {
+            accumulatedDelta = 0;
+            GameObject instantiatedObject = Instantiate(enemyPrefab);
+
+            Vector3 spawn = wolfSpawns[Random.Range(0, wolfSpawns.Length)].gameObject.transform.position;
+            instantiatedObject.transform.position = spawn;
+        }
+    }
+}
