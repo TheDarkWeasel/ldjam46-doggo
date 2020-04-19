3ac685ea5adcfb99262008327f67087d19498a82
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2020-04-19 23:38:42 +0200

    Title and Explanation

---
+++ Doggo/Doggo/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    bool loading = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            if(!loading)
+            {
+                loading = true;
+                GetComponent<AudioSource>().Play();
+                StartCoroutine(LoadNextScene());
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
